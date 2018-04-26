e627c090675754615e22fa11d8065d06e2306798
Keaton <linux.keaton@gmail.com>
2018-04-26 02:20:11 -0500

    Update delete me.py

--- delete me.py
+++ delete me.py
@@ -7,11 +7,14 @@ from re import search
 ########
 # Runs in bkg to interface with gnudip
 # Set up cron job for like once every hour. Less than that if I can find a service that returns my public IP through API
+######
+# Edit: It should be possible to poll my router for my public IP. Do testing when you get home
 
-currIP = post("https://gnudip.datasystems24.net/gnudip/cgi-bin/gnudip.cgi",form={"username":username,"password":password,"domain":"freedombox.rocks","page":"login","localaddr":"nojava","login":"Login"})
+#currIP = post("https://gnudip.datasystems24.net/gnudip/cgi-bin/gnudip.cgi",form={"username":username,"password":password,"domain":"freedombox.rocks","page":"login","localaddr":"nojava","login":"Login"}).text
+currIP = ""
 
-curr = search(r"(?:i)Currently Points to ([\d\.]+)").group(1)
-me = search(r"(?:i)GnuDIP has IP address ([\d\.]+)").group(1)
+curr = search(r"(?:i)Currently Points to ([\d\.]+)",currIP).group(1)
+me = search(r"(?:i)GnuDIP has IP address ([\d\.]+)",currIP).group(1)
 
 if curr != me:
     fixIP = post("https://gnudip.datasystems24.net/gnudip/cgi-bin/gnudip.cgi",form={"username":username,"password":password,"domain":"freedombox.rocks","page":"login","localaddr":"nojava","updatehost":"Go","updateaddr":IP})
